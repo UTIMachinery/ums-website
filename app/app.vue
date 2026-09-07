003ce1abc28ba5433de486d2b8740e05f27442d7
UTIMachinery <utimachinery@gmail.com>
2026-09-07 15:42:59 -0500

    Expose Mazak single-record model browser

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,7 @@
   <div>
     <SiteHeader />
     <NuxtPage />
+    <div v-if="showMazakIndex" class="mazak-single-wrap"><MazakSingleRecordLinks /></div>
     <EquipmentFooterCta v-if="showEquipmentDetailCta" />
   </div>
 </template>
@@ -12,4 +13,7 @@ const route = useRoute()
 const showEquipmentDetailCta = computed(() =>
   /^\/equipment\/[^/]+\/[^/]+/.test(route.path)
 )
+const showMazakIndex = computed(() => route.path === '/spec-library/mazak/cnc-lathes')
 </script>
+
+<style scoped>.mazak-single-wrap{max-width:1260px;margin:0 auto;padding:0 28px 40px}@media(max-width:850px){.mazak-single-wrap{padding:0 18px 30px}}</style>
