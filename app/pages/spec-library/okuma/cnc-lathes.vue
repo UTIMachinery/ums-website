5fcf21ed21975c20c8f778d2cc8186142fac8ad3
UTIMachinery <utimachinery@gmail.com>
2026-09-09 18:55:43 -0500

    Add Okuma CNC lathe library route

---
+++ app/pages/spec-library/okuma/cnc-lathes.vue
@@ -0,0 +1,4 @@
+<template><ManufacturerCncLatheLibrary manufacturer="Okuma" route-slug="okuma" :entries="entries" /></template>
+<script setup>
+import entries from '~/assets/data/okuma-historical-detailed.js'
+</script>
