3ce7100a6b0f118cba4af3b864b9694e95ef6555
UTIMachinery <utimachinery@gmail.com>
2026-09-09 18:55:58 -0500

    Add Mori-Seiki CNC lathe library route

---
+++ app/pages/spec-library/mori-seiki/cnc-lathes.vue
@@ -0,0 +1,4 @@
+<template><ManufacturerCncLatheLibrary manufacturer="Mori-Seiki" route-slug="mori-seiki" :aliases="['Mori Seiki']" :entries="entries" /></template>
+<script setup>
+import entries from '~/assets/data/moriseiki-historical-detailed.js'
+</script>
