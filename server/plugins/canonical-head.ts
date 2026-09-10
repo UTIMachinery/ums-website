967698d5bc6cdb3513613e41297da0d762828eb0
UTIMachinery <utimachinery@gmail.com>
2026-09-10 17:40:09 -0500

    Normalize canonical head URLs to www domain

--- server/plugins/canonical-head.ts
+++ server/plugins/canonical-head.ts
@@ -1,10 +1,13 @@
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('render:html', (html) => {
-    const oldBase = 'https://equipment.usedmachinerysource.com'
+    const oldEquipmentBase = 'https://equipment.usedmachinerysource.com'
+    const nonWwwBase = 'https://usedmachinerysource.com'
     const primaryBase = 'https://www.usedmachinerysource.com'
 
     html.head = html.head.map((chunk) =>
-      chunk.replaceAll(oldBase, primaryBase)
+      chunk
+        .replaceAll(oldEquipmentBase, primaryBase)
+        .replaceAll(nonWwwBase, primaryBase)
     )
   })
 })
