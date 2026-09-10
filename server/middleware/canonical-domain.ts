cbc85dac7214cb3b17f9f7648b131e512648e625
UTIMachinery <utimachinery@gmail.com>
2026-09-10 17:40:14 -0500

    Redirect all non-www traffic to canonical domain

--- server/middleware/canonical-domain.ts
+++ server/middleware/canonical-domain.ts
@@ -1,7 +1,10 @@
 export default defineEventHandler((event) => {
   const url = getRequestURL(event)
 
-  if (url.hostname === 'equipment.usedmachinerysource.com') {
+  if (
+    url.hostname === 'equipment.usedmachinerysource.com' ||
+    url.hostname === 'usedmachinerysource.com'
+  ) {
     return sendRedirect(
       event,
       `https://www.usedmachinerysource.com${url.pathname}${url.search}`,
