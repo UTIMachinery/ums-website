aa1a0969cf76d73a8c9be1b8b5c5f1dd76559b41
UTIMachinery <utimachinery@gmail.com>
2026-09-11 13:21:49 -0500

    Make Email Builder login session-only

--- server/api/email-builder-login.post.js
+++ server/api/email-builder-login.post.js
@@ -31,8 +31,7 @@ export default defineEventHandler(async (event) => {
     httpOnly: true,
     secure: true,
     sameSite: 'strict',
-    path: '/',
-    maxAge: 60 * 60 * 12
+    path: '/'
   })
 
   return { success: true }
