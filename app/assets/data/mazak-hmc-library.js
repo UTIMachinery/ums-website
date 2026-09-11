8471392fb7ede01b60633d20b24caf98850e67ce
UTIMachinery <utimachinery@gmail.com>
2026-09-11 17:55:50 -0500

    Complete Mazak repeat-model HMC aggregation

--- app/assets/data/mazak-hmc-library.js
+++ app/assets/data/mazak-hmc-library.js
@@ -1,4 +1,9 @@
 import p1 from './mazak-hmc-1.js'
 import p2 from './mazak-hmc-2.js'
 import p3 from './mazak-hmc-3.js'
-export default [...p1,...p2,...p3]
+import p4 from './mazak-hmc-4.js'
+import p5 from './mazak-hmc-5.js'
+import p6 from './mazak-hmc-6.js'
+import p7 from './mazak-hmc-7.js'
+import p8 from './mazak-hmc-8.js'
+export default [...p1,...p2,...p3,...p4,...p5,...p6,...p7,...p8]
