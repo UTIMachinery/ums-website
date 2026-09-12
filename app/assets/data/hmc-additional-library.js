e42fb4ceeb383d75fe86dcb3c4e65b03d2e2ef75
UTIMachinery <utimachinery@gmail.com>
2026-09-11 21:29:28 -0500

    Use complete HMC historical specification library

--- app/assets/data/hmc-additional-library.js
+++ app/assets/data/hmc-additional-library.js
@@ -1,11 +1,2 @@
-import moriSeiki from './mori-seiki-hmc.js'
-import makino from './makino-hmc.js'
-import haas from './haas-hmc.js'
-import cincinnati from './cincinnati-hmc.js'
-import okuma from './okuma-hmc.js'
-import toshiba from './toshiba-hmc.js'
-import toyoda from './toyoda-hmc.js'
-import okk from './okk-hmc.js'
-import remaining from './hmc-remaining-library.js'
-
-export default [moriSeiki,makino,haas,cincinnati,okuma,toshiba,toyoda,okk,...remaining]
+import library from './hmc-complete-library.js'
+export default library.filter(m => m.name !== 'Mazak')
