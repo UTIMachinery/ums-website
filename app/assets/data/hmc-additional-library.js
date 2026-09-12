23108e0931d57ddcb36f976d480359354943320b
UTIMachinery <utimachinery@gmail.com>
2026-09-11 20:02:08 -0500

    Complete core HMC manufacturer data index

--- app/assets/data/hmc-additional-library.js
+++ app/assets/data/hmc-additional-library.js
@@ -1,3 +1,9 @@
 import moriSeiki from './mori-seiki-hmc.js'
 import makino from './makino-hmc.js'
-export default [moriSeiki,makino]
+import haas from './haas-hmc.js'
+import cincinnati from './cincinnati-hmc.js'
+import okuma from './okuma-hmc.js'
+import toshiba from './toshiba-hmc.js'
+import toyoda from './toyoda-hmc.js'
+import okk from './okk-hmc.js'
+export default [moriSeiki,makino,haas,cincinnati,okuma,toshiba,toyoda,okk]
