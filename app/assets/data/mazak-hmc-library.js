fce089f3b6b3ebfc952dff1760fa93dce5d7a326
UTIMachinery <utimachinery@gmail.com>
2026-09-11 21:29:23 -0500

    Use complete Mazak HMC historical specs

--- app/assets/data/mazak-hmc-library.js
+++ app/assets/data/mazak-hmc-library.js
@@ -1,9 +1,2 @@
-import p1 from './mazak-hmc-1.js'
-import p2 from './mazak-hmc-2.js'
-import p3 from './mazak-hmc-3.js'
-import p4 from './mazak-hmc-4.js'
-import p5 from './mazak-hmc-5.js'
-import p6 from './mazak-hmc-6.js'
-import p7 from './mazak-hmc-7.js'
-import p8 from './mazak-hmc-8.js'
-export default [...p1,...p2,...p3,...p4,...p5,...p6,...p7,...p8]
+import library from './hmc-complete-library.js'
+export default library.find(m => m.name === 'Mazak')?.models || []
